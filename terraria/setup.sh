b21d5fe19882ae3d5bcee268e6ed244b62fcc206
tranarchy <tranarchy>
2025-05-27 15:06:13 +0200

    fixed setup script

--- terraria/setup.sh
+++ terraria/setup.sh
@@ -3,7 +3,11 @@
 TERRARIA_PATH=$HOME/.steam/steam/steamapps/common/Terraria
 
 cp -f lib64/* $TERRARIA_PATH/lib64
-sudo chmod +x $TERRARIA_PATH/lib64/*
+chmod +x $TERRARIA_PATH/lib64/*
+
+rm -f $TERRARIA_PATH/System*dll
+rm -f $TERRARIA_PATH/monoconfig
+rm -f $TERRARIA_PATH/monomachineconfig
 
 sudo cp terraria-arm64 /usr/local/bin
 sudo chmod +x /usr/local/bin/terraria-arm64
